9c018a936b4f7c977616b026c630660e63c15f3d
Anderson, Jevin(jevanderson) <jevanderson@paypal.com>
2020-12-12 09:55:23 -0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import { Link } from 'gatsby';
-import { connect } from 'react-redux';
-import { compose } from 'redux';
 
 import './index.scss';
 import Layout from '../components/layout';
 import Image from '../components/image';
 import Banner from '../components/pages/index/banner/banner';
-import { setState, withProvider } from '../utils/redux';
 import Profiles from '../components/pages/index/profiles/index.js';
 import { addFontAwesome } from '../utils/add-font-awesome';
 
@@ -21,12 +17,6 @@ class IndexPage extends Component {
     }
   }
 
-  componentDidMount() {
-    setState({
-      views: this.props.views + 1
-    });
-  }
-
   render = () => (
     <Layout>
       <Banner />
@@ -37,37 +27,50 @@ class IndexPage extends Component {
         <div className="row">
           <div className="col-12 col-md-6">Photo of Jevin</div>
           <div className="col-12 col-md-6">
-            <h4>Brief</h4>
+            <h4 id="am-brief">Brief</h4>
             <p>
-              Nullam quis risus eget urna mollis ornare vel eu leo. Maecenas faucibus mollis interdum. Cras justo odio,
-              dapibus ac facilisis in, egestas eget quam. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor
-              auctor. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+              I'm a professional developer working at <a href="https://www.paypal.com/">PayPal</a>. I work on{' '}
+              <a href="https://www.paypal.com/ppcreditapply/da/us/lander">PayPal's Credit offerings</a>. If you have{' '}
+              <a href="https://www.paypal.com/ppcreditapply/da/us/lander">PayPal Credit</a>, or one of{' '}
+              <a href="https://www.paypal.com/us/for-you/pay-in-4">PayPal's installment plans</a>, you've probably seen
+              my work. If you haven't, you might consider trying them out. I'd love to hear your feedback.
             </p>
             <h4>Less Brief</h4>
             <p>
-              Nullam id dolor id nibh ultricies vehicula ut id elit. Aenean eu leo quam. Pellentesque ornare sem lacinia
-              quam venenatis vestibulum. Etiam porta sem malesuada magna mollis euismod. Nullam quis risus eget urna
-              mollis ornare vel eu leo.
+              After High School I served five years in the{' '}
+              <a href="https://en.wikipedia.org/wiki/United_States_Marine_Corps">United States Marine Corps</a>. My
+              primary MOS was <a href="https://www.cool.navy.mil/usmc/enlisted/2621.htm">2621, Signals intelligence</a>.
+              My secondary MOS, was{' '}
+              <a href="https://en.wikipedia.org/wiki/Marine_Security_Guard">8151, Marine Security Guard</a>. I was
+              stationed at{' '}
+              <a href="https://www.iimef.marines.mil/Units/2nd-Radio-Battalion/">Second Radio Battalion(2nd Rad Bin)</a>
+              , <a href="https://www.lejeune.marines.mil/">Camp Lejeune, North Carolina</a> in 2004. By 2006 I had
+              transitioned into embassy duty, and was stationed at the{' '}
+              <a href="https://ca.usembassy.gov/embassy-consulates/ottawa/">American Embassy Ottawa</a>, in{' '}
+              <a href="https://en.wikipedia.org/wiki/Ottawa">Ottawa, Canada</a>. In 2007 I was stationed at the{' '}
+              <a href="https://mz.usembassy.gov/embassy/maputo/">American Embassy Maputo</a>, in{' '}
+              <a href="https://en.wikipedia.org/wiki/Maputo">Maputo, Mozambique</a>. And in 2008 I was stationed at the{' '}
+              <a href="https://en.wikipedia.org/wiki/Embassy_of_the_United_States,_Rome">American Embassy Rome</a>, in{' '}
+              <a href="https://en.wikipedia.org/wiki/Rome">Rome, Italy</a>.
             </p>
             <p>
-              Maecenas sed diam eget risus varius blandit sit amet non magna. Fusce dapibus, tellus ac cursus commodo,
-              tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Curabitur blandit tempus
-              porttitor. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Sed posuere consectetur
-              est at lobortis. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem
-              nec elit. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+              After leaving active service in the Marine Corps, I attended college at the{' '}
+              <a href="https://missouri.edu/">University of Missouri, Columbia(Mizzou)</a>. I was an active member of
+              the <a href="https://veterans.missouri.edu/">veterans' organization</a>, and encourage veterans to
+              consider joining if you're attending Mizzou. I graduated with a{' '}
+              <a href="http://catalog.missouri.edu/undergraduategraduate/collegeofengineering/computerscience/bs-computer-science/">
+                Bachelor of Science with a major in Computer Science
+              </a>
+              .
             </p>
             <p>
-              Vestibulum id ligula porta felis euismod semper. Nullam id dolor id nibh ultricies vehicula ut id elit.
-              Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi leo risus,
-              porta ac consectetur ac, vestibulum at eros. Cum sociis natoque penatibus et magnis dis parturient montes,
-              nascetur ridiculus mus. Etiam porta sem malesuada magna mollis euismod.
+              After college I worked for <a href="https://en.wikipedia.org/wiki/Esri">Esri</a> doing mobile and web
+              development. Afterwards, I worked at <a href="http://www.recruitingventures.com/">Recruiting Ventures</a>{' '}
+              doing web development. Currently, I work at PayPal, see <a href="#am-brief">brief</a>.
             </p>
-            <h4>Conclusion</h4>
             <p>
-              Donec id elit non mi porta gravida at eget metus. Vestibulum id ligula porta felis euismod semper. Donec
-              ullamcorper nulla non metus auctor fringilla. Nullam quis risus eget urna mollis ornare vel eu leo. Aenean
-              eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Maecenas faucibus mollis interdum.
-              Curabitur blandit tempus porttitor.
+              My beautiful <a href="http://www.jamelaanderson.com/">wife</a> and I married December 12th, 2016. Tibbers,
+              our Labradoodle, was born September 06, 2017. We welcomed our first son, Everest, April 17, 2020.
             </p>
           </div>
         </div>
@@ -86,15 +89,6 @@ class IndexPage extends Component {
       </Link>
     </Layout>
   );
-
-  static propTypes = {
-    views: PropTypes.number
-  };
 }
 
-const mapStateToProps = ({ views = 0 }) => ({ views });
-
-export default compose(
-  withProvider,
-  connect(mapStateToProps)
-)(IndexPage);
+export default IndexPage;
